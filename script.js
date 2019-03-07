4475d8276dd978110c7f0d0dac20fe29dd193f58
Ansel Smolund <smolu006@umn.edu>
2019-03-07 16:19:02 -0600

    update

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-var count1 = 0;
-function changeImage1(){
-  var imgs = ["../images/1-1.jpg","../images/1-2.jpg","../images/1-3.jpg",
-  "../images/1-4.jpg","../images/1-5.PNG","../images/1-6.PNG","../images/1-7.jpg",
-  "../images/1-8.jpg","../images/1-9.jpg","../images/1-10.jpg","../images/1-11.jpg"];
-  var currImg = document.getElementById('img-1').src;
-  var fileName = currImg.substring(currImg.lastIndexOf('/')+1);
-  if(count1 >= imgs.length){
-    count1 = 0;
-  }
-  document.getElementById('img-1').src = imgs[count1];
-  count1++;
-
-}
-window.setInterval(function(){
-  changeImage1();
-}, 2000);
+//
+// window.onload = function(){
+//   var size = 5;
+//   var width = innerWidth;
+//   var height = 600;
+//
+//   var cols = 5;
+//   var rows = 5;
+//
+//   var spaceX = Math.floor(width/cols);
+//   var spaceY = Math.floor(height/rows);
+//
+//   console.log(spaceX,spaceY);
+//
+//   var img = new Image();
+//   img.src = "images/1-1.jpg";
+//   img.onload = function(){
+//     var canvas = document.getElementById('canvas');
+//     var ctx = canvas.getContext('2d');
+//     canvas.width = width;
+//     canvas.height = height;
+//     var h = img.height;
+//     var w = img.width;
+//     for(var i = 0; i < rows; i++){
+//       for(var j = 0; j < cols; j++){
+//         ctx.drawImage(img,i * spaceX, j * spaceY,w/20,h/20);
+//         console.log(i * spaceX);
+//         console.log(j * spaceY);
+//       }
+//     }
+//   }
+//
+// }
